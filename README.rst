4c7dea5f632a35be13d7ac86b9d624b74da650ee
Jannis Leidel <jannis@leidel.info>
2010-12-25 18:43:48 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,14 @@
+==================
+django-sites-tools
+==================
+
+This is a Django app that provides a few tools that extend the contrib sites app.
+
+The main website for django-sites-tools is
+`github.com/jezdez/django-sites-tools`_ where you can also file tickets.
+
+You can also install the `in-development version`_ of django-sites-tools with
+``pip install django-sites-tools==dev`` or ``easy_install django-sites-tools==dev``.
+
+.. _github.com/jezdez/django-sites-tools: http://github.com/jezdez/django-sites-tools
+.. _in-development version: https://github.com/jezdez/django-sites-tools/tarball/develop#egg=django-sites-tools-dev
---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='django-sites-tools',
+    version='0.1.0',
+    description="A Django app that provides tools for the contrib sites app.",
+    author='Jannis Leidel',
+    author_email='jannis@leidel.info',
+    license='BSD',
+    url='http://github.com/jezdez/django-sites-tools/',
+    packages=['sites_tools'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
